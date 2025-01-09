8caa5e1671f974544a30fff1c2f79b2a8f499a7a
majid hatami maleki <majidhatamimaleki@outlook.com>
2025-01-09 16:38:57 +0330

    create header

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      {/* header */}
+      {/* main */}
+      {/* footer */}
+    </div>
+  );
+}
---
+++ src/layouts/AppLayout.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import App from "../App";
+
+export default function AppLayout() {
+  return <div>AppLayout</div>;
+}
